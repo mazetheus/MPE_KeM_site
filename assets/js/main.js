d61ce12f32831995f145e020d6ff848443ee6de5
AAgência Digital <46752516+aagencia@users.noreply.github.com>
2021-10-21 15:25:54 -0300

    form

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,3 +128,19 @@ setInterval(function () {
     secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit">Segundos</span>'; 
  
 }, 1000);
+
+$('#bootstrapForm').submit(function (event) {
+    event.preventDefault()
+    var extraData = {}
+    $('#bootstrapForm').ajaxSubmit({
+        data: extraData,
+        dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
+        error: function () {
+            // Submit of form should be successful but JSONP callback will fail because Google Forms
+            // does not support it, so this is handled as a failure.
+            alert('Form Submitted. Thanks.')
+            // You can also redirect the user to a custom thank-you page:
+            window.location = 'https://mpebrasil.com.br/'
+        }
+    })
+})
